c9d14735582ba0b5594922115052aa7bf7761e47
Wes Hendley <wrhendley@gmail.com>
2023-03-02 14:14:08 -0600

    Add getComputerChoice function

--- gamecode.js
+++ gamecode.js
@@ -3,11 +3,17 @@ Take input from user
     input rock, paper, or scissors
         if input is not one of those
             display error message and ask again
-Take input from computer
-    randomly select one of three options for rock paper or scissors
+
 Determine winner
     compare user input to computer input
     store winner in a variable
 Display message
-    victory screen if user wins, otherwise losing screen
+    victory screen if user wins, tie if same choice, otherwise losing screen
 */
+
+function getComputerChoice(){
+    // Take input from computer
+    // randomly select one of three options for rock paper or scissors and return result
+    // 0 = rock, 1 = paper, 2=scissors
+    return(Math.floor(Math.random()*3));
+}
