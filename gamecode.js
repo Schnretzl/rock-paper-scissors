0b8106f0462278242b426b9bf88f49c6d64a3ad1
Wes Hendley <wrhendley@gmail.com>
2023-03-02 14:04:47 -0600

    Add pseudocode to JS

---
+++ gamecode.js
@@ -0,0 +1,13 @@
+/*
+Take input from user
+    input rock, paper, or scissors
+        if input is not one of those
+            display error message and ask again
+Take input from computer
+    randomly select one of three options for rock paper or scissors
+Determine winner
+    compare user input to computer input
+    store winner in a variable
+Display message
+    victory screen if user wins, otherwise losing screen
+*/
