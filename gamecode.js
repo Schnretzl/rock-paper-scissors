21d52011be613fe3792d22712c9940a8cc6816c0
Wes Hendley <wrhendley@gmail.com>
2023-03-03 14:50:02 -0600

    Add player prompt.  Get output working for single game

--- gamecode.js
+++ gamecode.js
@@ -1,27 +1,56 @@
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+
+
+
 function getComputerChoice(){
     return(Math.floor(Math.random() * 3));
 }
 
-let playerSelection = "rock";
-let computerSelection = getComputerChoice();
+function getPlayerChoice(){
+    let input = prompt("Rock, paper, or scissors?");
+    while(input.toUpperCase() != ("ROCK" || "PAPER" || "SCISSORS")){
+        alert("Input must be rock, paper, or scissors!");
+        input = prompt("Rock, paper, or scissors?");
+    }
+
+    return(input.toUpperCase());
+}
+
+//----------------------------------------------------------------
 
 function playRound(playerSelection, computerSelection){
-    let playerSelectionUpper = playerSelection.toUpperCase();
+    
     let playerSelectionToInt;
-    if(playerSelectionUpper === "ROCK"){
-        playerSelectionToInt = 0;
+        if(playerSelection === "ROCK"){
+            playerSelectionToInt = 0;
+            //playerSelectionFirstUpper = "Rock";
+        }else if(playerSelection === "PAPER"){
+            playerSelectionToInt = 1;
+            //playerSelectionFirstUpper = "Paper";
+        }else if(playerSelection === "SCISSORS"){
+            playerSelectionToInt = 2;
+            //playerSelectionFirstUpper = "Scissors";
+        }
+    let playerSelectionFirstUpper = playerSelection.slice(0,1) + (playerSelection.toLowerCase()).slice(1);
+    let computerSelectionText;
+    switch(computerSelection){
+        case 0: computerSelectionText = "Rock"; break;
+        case 1: computerSelectionText = "Paper"; break;
+        case 2: computerSelectionText = "Scissors"; break;
+        default: "Error in computer selection";
     }
 
     if(playerSelectionToInt - computerSelection === 0){            //tie        
-        return("tie");
+        return("There was a tie!");
     }
     else if((playerSelectionToInt - computerSelection === 1) ||    //player win
             playerSelectionToInt - computerSelection === -2){
-                return("you win");       
+                return(`You win! ${playerSelectionFirstUpper} beats ${computerSelectionText}`);       
     }
     else if((playerSelectionToInt - computerSelection === -1) ||   //player lose
             (playerSelectionToInt - computerSelection === 2)){
-                return("you lose");            
+                return(`You lose! ${computerSelectionText} beats ${playerSelectionFirstUpper}`);           
     }
     else{
         return("there was an error");
