169507cbdd1653d82fe6d449d21aadb1a51df379
Wes Hendley <wrhendley@gmail.com>
2023-03-03 11:35:49 -0600

    Create new gamecode.js file

--- gamecode.js
+++ gamecode.js
@@ -1,41 +1,32 @@
-/*
-
-
-Determine winner
-    compare user input to computer input
-    store winner in a variable
-Display message
-    victory screen if user wins, tie if same choice, otherwise losing screen
-*/
-
 function getComputerChoice(){
-    // Take input from computer
-    // randomly select one of three options for rock paper or scissors and return result
-    // 0 = rock, 1 = paper, 2=scissors
-    return(Math.floor(Math.random()*3));
+    return(Math.floor(Math.random() * 3));
 }
 
-function playerSelection(){
-    // Take input from user
-    // input rock, paper, or scissors
-    // if input is not one of those
-    // display error message and ask again
-    let input = prompt("Rock, paper, or scissors?");
-    let upperInput = input.toUpperCase();
-    while(upperInput != "ROCK" && upperInput != "PAPER" && upperInput != "SCISSORS"){
-        console.log(`You entered ${input}.  You must choose either Rock, paper, or scissors!`);
-        input = prompt("Rock, paper, or scissors?");
-        upperInput = input.toUpperCase();
-    }
-    if(upperInput === "ROCK"){
-        return 0;
-    }
-    if(upperInput === "PAPER"){
-        return 1;
-    }
-    if(upperInput === "SCISSORS"){
-    }return 2;
+let playerSelection = "rock";
+let computerSelection = getComputerChoice();
 
-    console.log("Selection choice code did not work.")
-    return -1;
+function playRound(playerSelection, computerSelection){
+    let playerSelectionUpper = playerSelection.toUpperCase();
+    let playerSelectionToInt;
+    if(playerSelectionUpper === "ROCK"){
+        playerSelectionToInt = 0;
+    }
+
+    if(playerSelectionToInt - computerSelection === 0){            //tie        
+        return("tie");
+    }
+    else if((playerSelectionToInt - computerSelection === 1) ||    //player win
+            playerSelectionToInt - computerSelection === -2){
+                return("you win");       
+    }
+    else if((playerSelectionToInt - computerSelection === -1) ||   //player lose
+            (playerSelectionToInt - computerSelection === 2)){
+                return("you lose");            
+    }
+    else{
+        return("there was an error");
+        //error
+    }
 }
+    
+    console.log(playRound(playerSelection, computerSelection));
