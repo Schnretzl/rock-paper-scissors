0abe8aaeda50278e279fdfc0b9d53bd7be5df150
Wes Hendley <wrhendley@gmail.com>
2023-03-17 14:56:03 -0500

    Change win/loss message.  Clean up code

--- gamecode.js
+++ gamecode.js
@@ -4,28 +4,12 @@ let computerWins = 0;
 let ties = 0;
 let playerSelection;
 let computerSelection;
-// results
-const playerWinMsg = document.createTextNode("You win!");
-const cpuWinMsg = "Computer wins!";
 
 
 function getComputerChoice(){
     return(Math.floor(Math.random() * 3));
 }
 
-function getPlayerChoice(){
-    //let input = prompt("Rock, paper, or scissors?");
-    // let inputUpper = input.toUpperCase();
-    // while((inputUpper != "ROCK") 
-    //     && (inputUpper != "PAPER")
-    //     && (inputUpper != "SCISSORS")){
-    //     alert("Input must be rock, paper, or scissors!");
-    //     input = prompt("Rock, paper, or scissors?");
-    //     inputUpper = input.toUpperCase();
-    // }
-
-    // return(inputUpper);
-}
     let choice;
     const results = document.querySelector('#results');
 
@@ -87,32 +71,12 @@ function playRound(playerSelection, computerSelection){
     }
     else{
         return("there was an error");
-        //error
     }
 }
 
 function displayResults(){
     document.querySelector('#cpuWins').textContent = "CPU wins:" + computerWins;
     document.querySelector('#playerWins').textContent = "Player wins:" + playerWins;
-    playerWins === 5 ? document.querySelector('#finalWin').textContent = "Winner!" : "";
-    computerWins === 5 ? document.querySelector('#finalWin').textContent = "Loser!" : "";
+    playerWins === 5 ? document.querySelector('#finalWin').textContent = "Player wins!" : "";
+    computerWins === 5 ? document.querySelector('#finalWin').textContent = "CPU wins!" : "";
 }
-    
-
-function game(rounds){
-    /* for(let i = 0; i < rounds; i++){
-         playerSelection = getPlayerChoice();
-         computerSelection = getComputerChoice();
-         console.log(playRound(playerSelection, computerSelection));
-         console.log(`Current score:\nPlayer: ${playerWins}, Computer: ${computerWins}, Ties: ${ties}`);
-    } */
-
-}
-    /*play a round
-      determine winner and add 1 to their wins
-      move to next round
-      do this 5 times*/
-
-//-------------------------------------------------------------------------------------
-
-game(1);
